5f07ff669fe1f362f1a39be5ea9589378ffa4c10
gritman <gritman@126.com>
2017-05-29 15:23:13 +0800

    简单流示例

--- src/app/bind/bind.component.ts
+++ src/app/bind/bind.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-bind',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    // 观察这个流,筛选出偶数,算平方,打印到页面上
+    // from是创建流,filter和map也都是返回一个流
+    Observable.from([1, 2, 3, 4])
+      .filter(e => e % 2 == 0)
+      .map(e => e * e)
+      .subscribe(
+        e => console.log(e),
+        err => console.error(err),
+        () => console.log('结束啦')
+      );
+  }
 
   ngOnInit() {
   }
