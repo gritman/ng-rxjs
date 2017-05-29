1c752936728a394bb302e5611c49f45ab658cdc0
gritman <gritman@126.com>
2017-05-29 16:54:17 +0800

    用debounceTime来延迟响应

--- src/app/bind/bind.component.ts
+++ src/app/bind/bind.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {FormControl} from '@angular/forms';
-import 'rxjs/Rx';
+import 'rxjs/Rx'; // 因为不是NG框架的,所以单独引用
 
 @Component({
   selector: 'app-bind',
@@ -15,7 +15,7 @@ export class BindComponent implements OnInit {
 
   constructor() {
     this.searchInput.valueChanges
-      .debounceTime(500) // 输入过程中,暂停了500ms,才去处理这个流,而不是每输入一个字符就调用一次
+      .debounceTime(500) // 输入过程中,暂停了500ms,才去处理这个流,而不是每输入一个字符就调用一次,延迟响应
       .subscribe( stockCode => this.getStockInfo(stockCode) );
   }
 
