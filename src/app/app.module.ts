43a3e8a43cdc6f5fe7fe68581cba700c0262f3ca
gritman <gritman@126.com>
2017-05-29 14:50:50 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BindComponent } from './bind/bind.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindComponent
   ],
   imports: [
     BrowserModule,
